301b1dfcd532a66f4e32e591b7e33d6bf720676a
ThanhDanh <thanhdanhdo1701@gmail.com>
2024-11-26 15:47:34 +0700

    feat: improve login with JWT

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,4 +6,7 @@ const router = require("express").Router()
 //phần logic nằm trong controllers theo mô hình MVC
 router.post("/login", authControllers.loginUser)
 
+//REFRESH TOKEN
+router.post("/refresh", authControllers.requestRefreshToken)
+
 module.exports = router
