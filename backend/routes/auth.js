675dc0a13ab4dbb572cc5774e155d1bcc96d3510
ThanhDanh <thanhdanhdo1701@gmail.com>
2024-11-26 16:24:48 +0700

    commit to create pull request

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,7 @@ const authControllers = require("../controllers/authControllers")
 
 const router = require("express").Router()
 
+//REGISTER
 //phần logic nằm trong controllers theo mô hình MVC
 router.post("/register", authControllers.registerUser)
 
