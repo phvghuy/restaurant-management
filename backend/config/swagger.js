3620d63b39fba9fe8c162c1b9616a2e2c3386e47
Pham Huy <huypham9724@gmail.com>
2024-12-14 00:37:48 +0700

    add account management feature

--- backend/config/swagger.js
+++ backend/config/swagger.js
@@ -15,9 +15,38 @@ const options = {
                 url: 'http://localhost:3000', // URL của server
                 description: 'Local server'
             }
-        ]
+        ],
+        components: {
+            schemas: {
+                User: {
+                    type: 'object',
+                    properties: {
+                        id: {
+                            type: 'string',
+                            description: 'The unique identifier of the user'
+                        },
+                        fullName: {
+                            type: 'string',
+                            description: 'Full name of the user'
+                        },
+                        email: {
+                            type: 'string',
+                            description: 'Email address of the user'
+                        },
+                        phoneNumber: {
+                            type: 'string',
+                            description: 'Phone number of the user'
+                        },
+                        isActive: {
+                            type: 'boolean',
+                            description: 'Whether the user account is active'
+                        }
+                    },
+                },
+            },
+        },
     },
-    apis: ['./routes/*.js'] // Đường dẫn tới file định nghĩa API
+    apis: ['./routes/*.js'], // Đường dẫn tới file định nghĩa API
 };
 
 const specs = swaggerJsDoc(options);
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -15,4 +15,58 @@ router.post("/requestEmailUpdate", middlewareController.verifyToken, userControl
 // Xác thực và cập nhật email
 router.post("/verifyEmailUpdate", middlewareController.verifyToken, userController.verifyEmailUpdate);
 
+/**
+ * @swagger
+ * /v1/user:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+// Lấy danh sách người dùng
+router.get("/", userController.getUsers);
+
+/**
+ * @swagger
+ * /v1/user/{id}/activate:
+ *   patch:
+ *     summary: Activate a user account
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: User account activated
+ *       404:
+ *         description: User not found
+ */
+router.patch("/:id/activate", userController.activateUser);
+
+/**
+ * @swagger
+ * /v1/user/{id}/deactivate:
+ *   patch:
+ *     summary: Deactivate a user account
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user
+ *     responses:
+ *       200:
+ *         description: User account deactivated
+ *       404:
+ *         description: User not found
+ */
+router.patch("/:id/deactivate", userController.deactivateUser);
+
 module.exports = router
