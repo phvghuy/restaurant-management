4430aca4cd2c90387205084152555ba50c5dab9b
Pham Huy <huypham9724@gmail.com>
2024-12-16 03:02:29 +0700

    add reservation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import HomePage from "./pages/HomePage/HomePage";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Blog from "./pages/BlogPage/Blog";
+import Reservation from "./pages/Reservation/Reservation";
 
 function App() {
   return (
     <Router>
       <div className="App"> 
         <Routes>
-          <Route path="/" element={<Blog />} />
+          <Route path="/" element={<Reservation />} />
         </Routes>
       </div>
     </Router>
