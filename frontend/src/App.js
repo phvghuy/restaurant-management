b26659ec2a1a1508607fc2f4b66f04df0e12ae23
Pham Huy <huypham9724@gmail.com>
2024-12-16 02:51:53 +0700

    add blog page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import HomePage from "./pages/HomePage/HomePage";
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Blog from "./pages/BlogPage/Blog";
 
 function App() {
   return (
     <Router>
       <div className="App"> 
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<Blog />} />
         </Routes>
       </div>
     </Router>
