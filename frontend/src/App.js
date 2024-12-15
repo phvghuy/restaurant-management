69521179c21345ac9640cf0919c77cf054ed311a
Pham Huy <huypham9724@gmail.com>
2024-12-16 03:14:42 +0700

    add contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Blog from "./pages/BlogPage/Blog";
 import Reservation from "./pages/Reservation/Reservation";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
     <Router>
       <div className="App"> 
         <Routes>
-          <Route path="/" element={<Reservation />} />
+          <Route path="/" element={<Contact />} />
         </Routes>
       </div>
     </Router>
